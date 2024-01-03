2075e5f3f1a3cbf682915e74d90dafd21766a6da
R7Frank <r7frank@hotmail.com>
2024-01-03 01:02:12 -0300

    Criacao do projeto e da classe Client

---
+++ src/test/java/com/r7frank/clientregister/ClientregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r7frank.clientregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
